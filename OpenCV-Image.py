b9a2e8326d371ef57b30d26b6ca8ef72fd233404
Adithya2004 <74169466+Adithya2004@users.noreply.github.com>
2023-01-15 14:48:00 +0530

    Update OpenCV-Image.py

--- OpenCV-Image.py
+++ OpenCV-Image.py
@@ -1,15 +1,18 @@
 import cv2 as cv
 import pytesseract as pyt
-#import PIL.Image
 import re
 import numpy as np
 
 capture = cv.VideoCapture(0)
 cntr=0
+flag = False
+
 while True:
+
     ref,frame = capture.read()
     cv.imshow('Video',frame)
     cntr+=1
+
     if(cntr%30 == 0):
         gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
         ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
@@ -17,10 +20,15 @@ while True:
         text  = pyt.image_to_string(thresh, config = myconfig)
         regex = r'[0-9]{2}[a-zA-Z]{3}[0-9]{4}'
         match = re.findall(regex, text)
-        print(match)
+        if match:
+            print(match[0])
+            flag = True
 
-    if cv.waitKey(20) & 0xFF==ord('q'):
+    if ((cv.waitKey(20) & 0xFF==ord('q')) | flag):
         break
+
 capture.release()
 cv.destroyAllWindows()
+
+
 
