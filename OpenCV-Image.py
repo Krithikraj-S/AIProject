ed9354d4994ddf65bd83893653910a129c5caf5b
Krithikraj-S <krithikswimmer@gmail.com>
2023-01-15 14:31:06 +0530

    Created intial code

---
+++ OpenCV-Image.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import pytesseract as pyt
+#import PIL.Image
+import re
+import numpy as np
+
+capture = cv.VideoCapture(0)
+cntr=0
+while True:
+    ref,frame = capture.read()
+    cv.imshow('Video',frame)
+    cntr+=1
+    if(cntr%30 == 0):
+        gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+        myconfig = r"--psm 1 --oem 3"
+        text  = pyt.image_to_string(thresh, config = myconfig)
+        regex = r'[0-9]{2}[a-zA-Z]{3}[0-9]{4}'
+        match = re.findall(regex, text)
+        print(match)
+
+    if cv.waitKey(20) & 0xFF==ord('q'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
